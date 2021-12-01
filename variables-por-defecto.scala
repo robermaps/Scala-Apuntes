b59251f4f94a559bae70c2cbf56d5c0a5e09814e
Rober J <roberer_@outlook.es>
2021-12-01 15:46:25 +0100

    Create variables-por-defecto.scala

---
+++ variables-por-defecto.scala
@@ -0,0 +1,11 @@
+// Se pueden crear variables MUTABLES sin darles un valor
+// Scala puede asignar valores predeterminados según del tipo de dato indicado
+
+val x:String =_   //error
+
+var x:String =_   //null
+var x:Int =_      //0
+var x:Double =_   //0.0
+var x:Range =_    //null
+
+// Útil cuando no sabemos el valor que va a tomar pero queremos asegurarnos del tipo
