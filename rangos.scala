9d0bec264000561f1f4e07583e0e530e5acf4232
Rober J <roberer_@outlook.es>
2021-12-01 15:35:30 +0100

    Update rangos.scala

--- rangos.scala
+++ rangos.scala
@@ -18,8 +18,8 @@ ent(1)  //2
 minus(15)  //p 
 
 // Imprimir en bucle for 
-for (x <- ent){print(x)}  //12345678910
-for (x <- minus){print(x)}  //abcdefghijklmnopqrstuvwxyz
-for (x <- ent2){print(x)}  //123456789
-for (x <- ent3){print(x + " ")}  //0 5 10 15 20 25
-for (x <- minus3){print(x + " ")}  //a d g j m p s v y 
+for (x <- ent) print(x)  //12345678910
+for (x <- minus) print(x)  //abcdefghijklmnopqrstuvwxyz
+for (x <- ent2) print(x)  //123456789
+for (x <- ent3) print(x + " ")  //0 5 10 15 20 25
+for (x <- minus3) print(x + " ")  //a d g j m p s v y 
