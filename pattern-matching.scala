3a16bf407f0b010080b62ff1f8d51504e429c495
Rober J <roberer_@outlook.es>
2021-12-01 15:58:04 +0100

    Create pattern-matching.scala

---
+++ pattern-matching.scala
@@ -0,0 +1,8 @@
+val color = "yellow"
+
+color match {
+     | case "blue" => print("Azul")
+     | case "red"  => print("Rojo")
+     | case "yellow" => print("Amarillo")
+     | }   
+              //Amarillo
