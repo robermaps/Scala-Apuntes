417ae2a6acd486e53cf108219436167c2e8fb686
Rober J <roberer_@outlook.es>
2021-12-01 18:54:25 +0100

    Create cadenas.scala

---
+++ cadenas.scala
@@ -0,0 +1,18 @@
+// Un string o cadena es un array de caracteres
+// Los caracteres llevan comillas simples 'a' y los strings "comillas dobles"
+
+// Guardar una cadena como objeto
+val cadena = "Roberto"
+
+// Ver la longitud de la cadena
+cadena.length                    // val res1: Int = 7
+print(cadena.length)             // 7
+
+// Obtener el primer y último caracter
+cadena.head                      // R
+cadena.last                      // o
+
+// Mayúsculas y minúsculas
+cadena.toLowerCase               // roberto
+cadena.toUpperCase               // ROBERTO
+
