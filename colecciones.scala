bfb42139d966744bf1742b85a3d52d595a84ea62
Rober J <roberer_@outlook.es>
2021-12-02 21:36:50 +0100

    Create colecciones.scala

---
+++ colecciones.scala
@@ -0,0 +1,5 @@
+// Docs: https://docs.scala-lang.org/overviews/collections-2.13/introduction.html
+//Las colecciones de Scalas se encuentran en la clase scala.collection
+
+// Listas - scala.collection.immutable - todos los valores deben ser del mismo tipo
+val lista1 = list("montaña", "río", "pantano")
