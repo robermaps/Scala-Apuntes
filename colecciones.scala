c20ce54505e225f3cea3b4f688e264538dfa36a9
Rober J <roberer_@outlook.es>
2021-12-09 19:15:06 +0100

    Update colecciones.scala

--- colecciones.scala
+++ colecciones.scala
@@ -3,16 +3,23 @@
 
 // Listas - scala.collection.immutable - todos los elementos son del mismo tipo
 
-val lista1 = list("montaña", "río", "pantano")   // List[string]
-val lista2 = list(24, 76, 200)                   // List[Int]
-val lista3 = list("montaña", "río", 5)           // List[Any] - Any es la clase principal de Scala
+val lista1 = List("montaña", "río", "pantano")   // List[String]
+val lista2 = List(24, 76, 200)                   // List[Int]
+val lista3 = List("montaña", "río", 5)           // List[Any] - Any es la clase principal de Scala
+val lista4 = List()                              // List[Nothing] 
 
-val lista4 = list[String]("montaña", "río", 5)   // Error
+val lista5 = List[String]("montaña", "río", 5)   // Error
 
 lista1(0)   // val res1: String = montaña
 lista2(1)   // val res2: Int = 76
 lista3(2)   // val res3: Any = 5
 
-print(lista1.last)     // pantano
-print(lista1.head)     // montaña
-print(lista2.length)   // 3
+print(lista1.last)      // pantano
+print(lista1.head)      // montaña
+print(lista2.length)    // 3
+print(lista4.isEmpty)   // true
+
+for (z <- lista1) println(z)   // montaña
+                               // pantano
+                               // río
+
