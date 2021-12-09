e2cd60ffbdb4912c213c4f844364e55b40307838
Rober J <roberer_@outlook.es>
2021-12-09 23:51:04 +0100

    Update colecciones.scala

--- colecciones.scala
+++ colecciones.scala
@@ -1,7 +1,8 @@
 // Docs: https://docs.scala-lang.org/overviews/collections-2.13/introduction.html
 // Las colecciones de Scalas se encuentran en la clase scala.collection
 
-//** LISTAS - scala.collection.immutable - todos los elementos son del mismo tipo
+//** LISTAS 
+//** scala.collection.immutable - todos los elementos son del mismo tipo
 
 // Crear listas
 val lista1 = List("montaña", "río", "pantano")   // List[String]
@@ -11,8 +12,9 @@ val lista4 = List()                              // List[Nothing]
 
 val lista5 = List[String]("montaña", "río", 5)             // Error
 val lista55 = List[String]("montaña", "río", "pantano")    // List[String]
+val lista555 = List[Char]('a', 'b', 'c')                   // List[Char]
 
-var lista6 = List.fill(4)("geo")   // List[String] = List(geo, geo, geo, geo)
+var lista6 = List.fill(4)("geo")   // List[String] = List(geo, geo, geo, geo) 
 
 // Posiciones
 lista1(0)   // val res1: String = montaña
@@ -38,13 +40,16 @@ lista6 = 55+:lista6             // ERROR - type mismatch
 lista6 = "geografía"+:lista6    // List(geografía, geo, geo, geo, geo, geografía)
 
 // Unir listas
-lista1:::lista2   // List("montaña", "río", "pantano", 24, 76, 200) - Cambia a tipo List[Any] al combinar enteros con cadenas
+lista1:::lista2      // List("montaña", "río", "pantano", 24, 76, 200) - Cambia a tipo List[Any] al combinar enteros con cadenas
 
 // Anidar listas
-lista1::lista2    // List(List("montaña", "río", "pantano"), 24, 76, 200) - List[Any]
+lista1::lista2       // List(List("montaña", "río", "pantano"), 24, 76, 200) - List[Any]
 
+// Comparar listas
+lista1 == lista2     // false
 
-//** LIST BUFFER - scala.collection.mutable - Pueden modificarse directamente
+//** LIST BUFFER
+//** Pueden modificarse directamente
 import scala.collection.mutable.ListBuffer
 val lista7 = ListBuffer( 1, 2, 3, 4, 5, 6)
 
@@ -68,7 +73,8 @@ lista8.remove(0, 2)
 print(lista8)         // ListBuffer(65) - La última posición no la borra
 
 
-//** ARRAY - la longitud de la lista no se puede cambiar (no se pueden añadir o borrar elementos)
+//** ARRAY 
+//** la longitud de la lista no se puede cambiar (no se pueden añadir o borrar elementos)
 val array1 = Array("montaña", "río", "pantano")
 
 // Especificar el tipo de array
@@ -85,9 +91,17 @@ val array44:Array[Int] = new Array(array2.size)        // Array(0, 0, 0, 0) - Di
 
 // Crear arrays con un rango
 val array5:Array[Int] = (1 to 5).toArray[Int]          // Array(1, 2, 3, 4, 5) - Convierte un rango en array y lo guarda en el nuevo val
+val array6:Array[Int] = (11 to 17).toArray[Int]        // Array(11, 12, 13, 14, 15, 16, 17)
 
 // Sustituir valores de un array dentro de otro
+// Se copiarán 2 posiciones de la array5 empezando
+// por la 0 en la array6 a partir de la posición 3
+Array.copy(array5, 0, array6, 3, 2)
+print(array6)                         // Array(11, 12, 13, 1, 2, 16, 17)
 
+// Concatenar arrays
+Array.concat(array5, array6)          // Array(1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 16, 17)
 
-
+//** ARRAY BUFFER 
+//**  
 
