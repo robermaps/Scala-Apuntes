df63a5a7e9167a8388d3eeb4ca22482fc394b139
Rober J <roberer_@outlook.es>
2021-12-11 19:18:14 +0100

    Update funciones.scala

--- funciones.scala
+++ funciones.scala
@@ -1,4 +1,5 @@
-// Crear funciones sin argumentos
+//** Crear funciones sin argumentos **\\
+
 def funcion1(): String = "Hola"           // val res1: String = Hola
 def funcion2 = "Adiós"                    // val res2: String = Adiós - Definir sin paréntesis solo si la funcion es simple y no hace llamadas a otras funciones
 
@@ -12,7 +13,20 @@ def funcion4() = print("Adiós")
 función3   //
 función4   // Adiós - Devuelve el resultado de la función print(), no un valor string
 
-// Crear funciones con argumentos
+
+//** Crear funciones con argumentos **\\
+
+// Media de dos números
 def media(num1: Float, num2: Float): Float = {(num1 + num2)/2}
 
 media(10, 15)    // val res3: Float = 12.5
+
+// Texto de presentación
+def presentacion(nombre: String, edad: Int, mayusculas: Boolean)=
+{
+  var frase = s"Me llamo $nombre y tengo $edad años"
+  if (mayusculas == true) frase = frase.toUpperCase
+  print(frase)
+}
+
+presentacion("Roberto", 26, true)     // ME LLAMO ROBERTO Y TENGO 26 AÑOS
