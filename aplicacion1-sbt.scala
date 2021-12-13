0595cf3c4daf2d9a90c495638ea104c2893bec0b
Rober J <roberer_@outlook.es>
2021-12-13 20:00:50 +0100

    Create aplicacion1-sbt.scala

---
+++ aplicacion1-sbt.scala
@@ -0,0 +1,11 @@
+// Al crear un proyecto se crean una serie de directorios. Esta aplicación será de un proyecto tipo sbt
+
+// Ficheros del proyecto sbt :
+// - .idea -> Ficheros del IDE IntelliJ
+// - build.sbt -> Archivo con las properties del proyecto, se mantendrá automáticamente
+// - target -> Donde se guardarán los resultados del proyecto
+// - src -> Código fuente. Tiene dos carpetas: /main (código fuente) y /test y cada una con una carpeta /scala
+
+// Para construir aplicaciones en scala tendremos que crear un paquete (package) en la carpeta src/main/scala
+// Después tendremos que crear dentro de ese package el primer objeto, que será el que ejecute la aplicación. 
+
