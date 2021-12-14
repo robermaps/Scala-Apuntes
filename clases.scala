e487f8cf24444ffed5663e313c540c671ecdd9ac
Rober J <roberer_@outlook.es>
2021-12-14 23:23:05 +0100

    Update clases.scala

--- clases.scala
+++ clases.scala
@@ -1,12 +1,13 @@
-// Crear una clase
-class Ciudad(nombre: String, altitud: Int, superficiekm2: Float) {        // Constructor - OPCIONAL -> Parámetros obligatorios para crear la clase (private properties) 
+//** Crear una clase PRINCIPAL **\\
+class Ciudad(nombre: String, altitud: Int, superficiekm2: Double) {        // Constructor - OPCIONAL -> Parámetros obligatorios para crear la clase (private properties) 
   
-  def resumen(): Unit =                                                   // Método de clase
+  def resumen(): Unit = {                                                  // Método de clase
     println("Nombre: " + nombre)
     println("Altitud: " + altitud)
     println("Superficie: " + superficiekm2)
+  }
   
-  var poblacion:Int = _                                                   // Variable de la clase (public property)
+  var poblacion:Int = _                                                    // Variable de la clase (public property)
   
 }
 
@@ -19,10 +20,40 @@ ciudad1.resumen()  // Altitud: 650
                    // Nombre: Madrid
 
 // Ver la clase del objeto ciudad1 a través de un método público
-print(ciudad1.getClass)   // class package.Ciudad - se supone que hemos creado esta clase en un package dentro de nuestro proyecto
+print(ciudad1.getClass)       // class package.Ciudad - se supone que hemos creado esta clase en un package dentro de nuestro proyecto
 
 // Editar la variable de la clase y visualizarla
 ciudad1.poblacion = 3223000
 print(ciudad1.poblacion)      // 3223000
 
 
+//** Crear una clase HIJA o SUBCLASE **\\
+// Tomará los métodos y properties de la clase principal y lo ampliará con los suyos propios
+class Barrio extends Ciudad("Granada", 738, 88.02) {           // Debe usarse el constructor de la clase principal
+  
+  var nombreBarrio: String = _     
+  var postal: Integer = _  
+  
+  def resumenBarrio() : Unit = {
+    println(s"El barrio $nombreBarrio tiene el código postal $postal y una población de $poblacion habitantes")
+  }
+}
+
+// Crear objeto de la subclase
+var barrio1 = new Barrio         // No tiene parámetros porque no tiene constructor. Sin embargo ha tomado los que se definieron al crear la subclase Barrio
+
+// Usar el método resumen() heredado de la clase principal
+barrio1.resumen()  // Altitud: 738
+                   // Superficie: 88.02
+                   // Nombre: Granada
+
+// Ver la clase del objeto ciudad1 
+print(barrio1.getClass)       // class package.Barrio
+
+// Editar las variables del objeto 
+barrio1.nombreBarrio = "Albaicín"
+barrio1.postal = 18010      
+barrio1.poblacion = 8277      // Variable heredada
+
+// Método exclusivo de la subclase
+barrio1.resumenBarrio()       // El barrio Albaicín tiene el código postal 18010 y una población de 8277 habitantes
