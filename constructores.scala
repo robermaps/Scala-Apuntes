9fa241f9c8500cbc5c6a1df1eb1d02c161eff98d
Rober J <roberer_@outlook.es>
2021-12-16 20:15:11 +0100

    Update constructores.scala

--- constructores.scala
+++ constructores.scala
@@ -1,3 +1,5 @@
+                                                                       //--** EJEMPLO 1 **--\\
+
 // Crear una clase 
 class Ciudad(nombre: String, clientes: Int) {               // Este es el CONSTRUCTOR PRINCIPAL
 
@@ -19,7 +21,7 @@ class Ciudad(nombre: String, clientes: Int) {               // Este es el CONSTR
 }
 
 // Correr el código
-object hola {
+object prueba1 {
   def main(args: Array[String]): Unit = {
     
     var ciudad1 = new Ciudad("Córdoba", 200)                // La ciudad Córdoba ha sido creada (CONSTRUCTOR PRINCIPAL - el valor de presupuesto y de ratio será 0)
@@ -31,3 +33,56 @@ object hola {
                                                             // El ratio presupuesto/cliente para Segovia es de 250
   }
 }
+
+
+
+                                                                       //--** EJEMPLO 2 **--\\
+
+class Ciudad {                                        // CONSTRUCTOR PRINCIPAL SIN ARGUMENTOS
+  
+  println("Ciudad creada")
+  private var nombre:String = _                 
+  private var altitud:Int = _  
+  private var clientes:Int = _                 
+  private var presupuesto:Int = _  
+  
+  def this(nombre: String, altitud: Int) {            // CONSTRUCTOR AUXILIAR 1
+    this()                                            // Referencia al constructor principal
+    this.nombre = nombre                              
+    this.altitud = altitud   
+  }
+  
+  def this(clientes: Int, presupuesto: Int) {         // CONSTRUCTOR AUXILIAR 2
+    this()                                            // Referencia al constructor principal. 
+                                                      // SI SE QUISIESE PODRÍA REFERENCIARSE AL OTRO CONSTRUCTOR AUXILIAR PARA INICIARLO TAMBIÉN PERO DEBEN COINCIDIR LOS PARÁMETROS
+    this.clientes = clientes                              
+    this.presupuesto = presupuesto
+  }  
+  
+  def resumen1(): Unit ={                                                                                   // Método de clase
+    println(s"La ciudad $nombre tiene una altitud de $altitud")
+  }
+
+  def resumen2(): Unit ={                                                                                   // Método de clase
+    println(s"Hay $clientes clientes y un presupuesto de $presupuesto")
+  }
+  
+}
+
+
+// Correr el código
+object prueba2 {
+  def main(args: Array[String]): Unit = {
+    
+    var ciudad1 = new Ciudad1("Lugo", 465)    // Ciudad creada
+    var ciudad2 = new Ciudad1(25, 6000)       // Ciudad creada
+    
+    ciudad1.resumen1()                        // La ciudad Lugo tiene una altitud de 465
+    ciudad1.resumen2()                        // Hay 0 clientes y un presupuesto de 0
+
+    ciudad2.resumen1()                        // La ciudad null tiene una altitud de 0
+    ciudad2.resumen2()                        // Hay 25 clientes y un presupuesto de 6000
+
+  }
+}
+
