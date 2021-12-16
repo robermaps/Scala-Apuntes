2db46c59b1e338fc20734cb551def4f16f3d4c64
Rober J <roberer_@outlook.es>
2021-12-16 15:20:19 +0100

    Update traits.scala

--- traits.scala
+++ traits.scala
@@ -3,7 +3,6 @@
 
 // Crear un Trait
 trait trait1 {  
-  var altitud: Integer = _
   
   def Imprimir(nombre: String): Unit = {
     var texto = s"La montaña se llama $nombre"
@@ -18,11 +17,6 @@ class Montaña extends trait1
 var monte1 = new Montaña()
 monte1.Imprimir("Veleta")     // LA MONTAÑA SE LLAMA VELETA
 
-// Acceder a una variable del trait
-monte1.altitud = 3394
-val pico = monte1.altitud
-print(pico)                   // 3394
-
 // Modificar un método definido en el trait para que no lo imprima en mayúscula
 class Montaña extends trait1 {
   
@@ -32,4 +26,27 @@ class Montaña extends trait1 {
     
   } 
 }
+
+// Implementar varios traits a una clase (en Scala no hay herencia múltiple)
+// Crear un nuevo trait
+trait trait2 {
+  def Imprimir2(provincia: String): Unit = {
+    println(s"Está ubicada en la provincia de $provincia")
+  }
+}
+
+trait trait3 {
+  var altitud: Integer = _
+  def Imprimir3(): Unit = {
+    println(s"Tiene una altitud de $altitud metros")
+  }
+}
+
+// Crear una clase que tome los traits 1 y 2
+class Montaña extends trait1 with trait2 with trait3
+
+// Usar los métodos y variables del los traits 2 y 3
+monte1.altitud = 3394
+monte1.Imprimir2("Granada")  // Está ubicado en la provincia de Granada
+monte1.Imprimir3()           // Tiene una altitud de 3394 metros
 
