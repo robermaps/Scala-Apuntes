0ae366528e529e4b4ba5cbbe36537057eb47a0ce
Rober J <roberer_@outlook.es>
2021-12-17 15:12:11 +0100

    Create singleton-objects.scala

---
+++ singleton-objects.scala
@@ -0,0 +1,13 @@
+// Los objetos singleton son el equivalente a las clases estáticas de otros lenguajes
+// Estos objetos NO PUEDEN INSTANCIARSE, es decir, no se pueden crear variables de esta clase con el comando - new - 
+// Todos los métodos de un singleton object son públicos = accesibles desde todo el programa
+// No puedo pasarle parámetros para el constructor primario
+// Pueden extender otras clases y traits, es decir, pueden adoptar variables y métodos de otras clases y traits
+
+// Crear un singleton object
+object rio {
+  
+  
+}
+
+// Singleton object como entrada a la aplicación
