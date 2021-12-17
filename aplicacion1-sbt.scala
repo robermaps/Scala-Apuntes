363c146138dab5625eee40ee22c2387b12311ea9
Rober J <roberer_@outlook.es>
2021-12-17 14:40:26 +0100

    Update aplicacion1-sbt.scala

--- aplicacion1-sbt.scala
+++ aplicacion1-sbt.scala
@@ -7,23 +7,24 @@
 // - src -> Código fuente. Tiene dos carpetas: /main (código fuente) y /test y cada una con una carpeta /scala
 
 // Para construir aplicaciones en scala tendremos que crear un paquete (package) en la carpeta src/main/scala (el de ejemplo se llamará prueba1)
-// Después tendremos que crear dentro de ese package el primer object (New/Scala class/Object) que será el que ejecute la aplicación. 
+// Después tendremos que crear dentro de ese package un object (New/Scala class/Object) que será el que ejecute la aplicación. 
 // Se puede hacer de dos maneras:
 
-// 1 - Objeto que con extends App (trait) 
+// 1 - Objeto con extends App (trait) 
 package prueba1
 
-object hola extends App {
+object aplicacion1 extends App {
   println("Hola Mundo")
 }
 
-// 2 - Con una función main dentro de nuestro object
+// 2 - Objeto con una función main (más habitual)
 package prueba1
 
-object hola {
+object aplicacion2 {
   def main(args: Array[String]): Unit =  
-    println("Hola Mundo")
+    println("Hola Mundo")                  
 }
 
-
+// El código que se ejecutará al pulsar ▶ Run será el que se encuentre dentro de del objeto creado
+// Las clases, traits y demás elementos se definirán por lo general antes del objeto, o en su defecto en otros packages o archivos del proyecto
 
