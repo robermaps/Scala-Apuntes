789cbd393e07ccb0dea67333cd13baccecb1d9a3
Rober J <roberer_@outlook.es>
2021-12-20 21:28:34 +0100

    Update companion-objects.scala

--- scala-core/companion-objects.scala
+++ scala-core/companion-objects.scala
@@ -12,14 +12,14 @@ class parcela(superficie:Double, valor:Int, tipo:String) {
   
   var factor:Double = _
   
-  def precio():Double = {
+  def precio_metro():Double = {
     tipo match {
       case "RURAL" => factor = 0.7
       case "URBANIZABLE" => factor = 1
       case "URBANO" => factor = 1.2
     }
 
-    (valor/superficie)*factor
+    (valor/superficie)*factor        // Devuelve un valor Double
     
   }
 }
@@ -28,22 +28,27 @@ class parcela(superficie:Double, valor:Int, tipo:String) {
 // Companion object para predefinir el parámetro 'tipo'
 // Debe tener el mismo nombre que la clase anterior
 object parcela {
-  val tipo_rural = "RURAL"
+  val tipo_rural = "RURAL"                                                      // Variables estáticas
   val tipo_urbano = "URBANO"
   val tipo_urbanizable = "URBANIZABLE"
+  
+  def ponderaciones():Unit = {                                                  // Método estático
+    println("Ponderaciones: Rural = 0.7 | Urbanizable = 1 | Urbano = 1.2")
+  }   
 }
 
 // main
 object main {
   def main(args: Array[String]):Unit ={
     
-    var parcela1 = new parcela(500,3000, parcela.tipo_urbano)    
+    var parcela1 = new parcela(500,3000, parcela.tipo_urbano)        // Parámetro tipo tomado de la variable estática 
     
-    parcela1.resumen()            // Superficie: 500.0
-                                  // Valor: 3000
-                                  // Tipo: URBANO
+    parcela1.resumen()                  // Superficie: 500.0
+                                        // Valor: 3000
+                                        // Tipo: URBANO
     
-    println(parcela1.precio())    // 7.199999999999999
+    println(parcela1.precio_metro())    // 7.199999999999999
     
+    parcela.ponderaciones()             // Ponderaciones: Rural = 0.7 | Urbanizable = 1 | Urbano = 1.2
   }
 }
