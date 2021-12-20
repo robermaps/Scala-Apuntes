cfec5d4d4b1b698ea5696ab0931c1bbffc22d9a8
Rober J <roberer_@outlook.es>
2021-12-20 16:59:30 +0100

    Update singleton-objects.scala

--- scala-core/singleton-objects.scala
+++ scala-core/singleton-objects.scala
@@ -5,9 +5,20 @@
 // Pueden extender otras clases y traits, es decir, pueden adoptar variables y métodos de otras clases y traits
 
 // Crear un singleton object
-object rio {
-  
-  
+object españa {
+  val poblacion = 47
+  val superficie = 506
+  def resumen():Unit = {
+    println(s"España tiene una población de $poblacion millones y una superficie de $superficie mil Km2")
+  }
 }
 
 // Singleton object como entrada a la aplicación
+object entrada {
+  def main(args: Array[String]): Unit = {
+    // código de la aplicación
+    println(españa.poblacion)               // 47
+    españa.resumen()                        // España tiene una población de 47 millones y una superficie de 506 mil Km2
+    val pais = new españa()                 // not found: type españa - ERROR 
+  }
+}
