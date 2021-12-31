62af72deac3840a379a4ad8046c76377e0c407b7
Rober J <roberer_@outlook.es>
2021-12-31 12:15:14 +0100

    Update and rename companion-instancias.scala to instancia-sin-new.scala

---
+++ scala-core/instancia-sin-new.scala
@@ -0,0 +1,32 @@
+// Se pueden realizar instancias sin NEW definiendo un método apply dentro de un companion object
+// Ejemplo:
+
+// Clase principal
+class parcela(superficie:Double, valor:Int, tipo:String) {
+
+  def resumen():Unit = {
+    println(s"Superficie: $superficie | Valor: $valor | Tipo: $tipo")
+  }
+
+}
+
+// Companion object
+object parcela {
+
+  val tipo_rural = "RURAL"
+  val tipo_urbano = "URBANO"
+  val tipo_urbanizable = "URBANIZABLE"
+
+  // Método apply que devuelve un objeto del tipo de la clase principal
+  def apply(superficie:Double, valor:Int, tipo:String): parcela = {      // Toma los mismos argumentos que el constructor principal y devuelve un objeto del tipo parcela
+    new parcela(superficie:Double, valor:Int, tipo:String)               // Genera el objeto con los valores
+  }
+}
+
+// Main
+object main1 {
+  def main(args: Array[String]):Unit ={
+    var parcela1 = parcela(750.5 ,45000 , parcela.tipo_rural)    // Variable definida SIN NEW
+    parcela1.resumen()                                            // Superficie: 750.5 | Valor: 45000 | Tipo: RURAL
+  }
+}
