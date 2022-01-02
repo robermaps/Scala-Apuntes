0ea71e8b242aa21263861a19628dd5b21c3fc112
Rober J <roberer_@outlook.es>
2022-01-02 21:45:53 +0100

    Update funciones-como-variables.scala

--- scala-core/funciones-como-variables.scala
+++ scala-core/funciones-como-variables.scala
@@ -1,8 +1,9 @@
-// Ejemplo 1
+// Ejemplo 1 - Media de dos números
 val media = (num1:Float,num2:Float) => {(num1+num2)/2}
 println(media(50,39))   // 44.5
 
-// Ejemplo 2
+// Ejemplo 2 - Jugar a cara o cruz
+import scala.math.random
 val caraocruz = (num:Int)  => for (z <- 1 to num) { if (random >= 0.5) println("Cara") else println("Cruz")}
 caraocruz(5)     // Cruz
                  // Cruz
@@ -10,7 +11,14 @@ caraocruz(5)     // Cruz
                  // Cruz
                  // Cruz
 
-// Ejemplo 3 © Apasoft Training
+// Ejemplo 3 © Apasoft Training - Filtrar los números pares de una lista
 val lista1 = List.range(1,10)
 val par = (num:Int) => num%2==0
 println(lista1.filter(par))
+
+// Ejemplo 4 - Números aleatorios entre 0 y el número pasado como parámetro
+import scala.math.random
+def randomulti = {(x:Int) => (x*random).round}
+val aleatorio = randomulti
+println(aleatorio(100))    // 58
+println(aleatorio(45))     // 33
