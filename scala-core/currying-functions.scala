395eb6fa51d2a89b4e0bb8b40f17a50a1a7db09f
Rober J <roberer_@outlook.es>
2022-01-09 14:50:10 +0100

    Update currying-functions.scala

--- scala-core/currying-functions.scala
+++ scala-core/currying-functions.scala
@@ -1,6 +1,6 @@
-// Las funciones curry permiten transformar funciones con varios argumentos en funciones de un único argumento
+// Las funciones curry permiten transformar funciones con varios argumentos en funciones de argumentos simples
 
-// Las siguientes funciones son iguales:
+// Las siguientes funciones devuelven el mismo resultado:
 def media (num1:Int, num2:Int) = println((num1+num2)/2)
 def media_curry (num1:Int) (num2:Int) = println((num1+num2)/2)
 
@@ -8,3 +8,13 @@ def media_curry (num1:Int) (num2:Int) = println((num1+num2)/2)
 media(56, 88)          // 72
 media_curry(56)(88)    // 72 
 
+// De un modo más formal, las currying functions pueden definirse de la siguiente manera:
+def media_curry2 (num1:Int): Int => Unit = (num2:Int) => println((num1+num2)/2)
+
+// Una buena forma de aprovechar estas funciones es con las funciones parciales (partially applied functions):
+val media1 = media_curry(30)_  
+media1(50)    // 40 
+
+// Cuando escribimos estas funciones de la manera 'formal' podemos prescindir del _
+val media2 = media_curry2(30)
+media2(50)    // 40
